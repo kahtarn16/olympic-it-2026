d2af947351f2bd0e89a8a7d81dea86b5d922f361
kahtarn16 <kha2005.it@gmail.com>
2026-06-15 13:52:34 +0700

    feat: initial project setup, configure database and redis properties

---
+++ backend/olympic_it_project/src/main/java/org/example/olympic_ot_project/OlympicOtProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.olympic_ot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlympicOtProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlympicOtProjectApplication.class, args);
+    }
+
+}
---
+++ backend/olympic_it_project/src/test/java/org/example/olympic_ot_project/OlympicOtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.olympic_ot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlympicOtProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
